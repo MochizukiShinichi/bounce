ca5240c988c612202a1e5182f704d9fe26c73a6c
jiangnan8910@gmail.com <jiangnan8910@gmail.com>
2016-02-09 15:24:01 -0800

    version 2.0
    result of 3d and 4d obtained

--- bounceFFT.m
+++ bounceFFT.m
@@ -20,34 +20,35 @@ for i = 1:Lf
     SE(i) = 2*(pi^2)*r(1)*dot(SEdensity,r.^3);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % CE 
-    hk = 1e-2;
+    hk = 1e-4;
     k_end = 100.0/r(end);
     k=linspace(hk,k_end,2.0^10);
     k = k';
+    hk = k(2)-k(1);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % 4D version
-    % fft = ((k.^(-1))*(r'.^2).*besselj(1,k*r'))*phi;
-    % fk=fft.*conj(fft);
-    % fkmax = max(fk);
-    % fk = fk/fkmax;
-    % CEdensity = (fk.*log(fk)).*(k.^(3));
-    % CE(i)=-2*pi*pi*hk*sum(CEdensity);
-    % filename = strcat('epsilon',num2str(epsilon,'%.2f'),'kfile','.mat');
-    %     save(filename,'epsilon','k','fft','fk','CEdensity');
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % 3D version
-    fft = ((k.^(-0.5))*(r'.^1.5).*besselj(0.5,k*r'))*phi;
+    fft = ((k.^(-1))*(r'.^2).*besselj(1,k*r'))*phi;
     fk=fft.*conj(fft);
     fkmax = max(fk);
     fk = fk/fkmax;
-    CEdensity = (fk.*log(fk)).*(k.^2);
-    CE(i)=-4*pi*hk*sum(CEdensity);
+    CEdensity = (fk.*log(fk)).*(k.^(3));
+    CE(i)=-2*pi*pi*hk*sum(CEdensity);
+    filename = strcat('epsilon',num2str(epsilon,'%.2f'),'kfile','.mat');
+    save(filename,'epsilon','k','fft','fk','CEdensity');
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % 3D version
+%     fft = ((k.^(-0.5))*(r'.^1.5).*besselj(0.5,k*r'))*phi;
+%     fk=fft.*conj(fft);
+%     fkmax = max(fk);
+%     fk = fk/fkmax;
+%     CEdensity = (fk.*log(fk)).*(k.^2);
+%     CE(i)=-4*pi*hk*sum(CEdensity);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % result data process
-% CE = (CE-mean(CE))/std(CE);
-% SE = (SE-mean(SE))/std(SE);
+CE = (CE-mean(CE))/std(CE);
+SE = (SE-mean(SE))/std(SE);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-save('result.mat','Epsilon','Phic','CE','SE');
+save('result_4d.mat','Epsilon','Phic','CE','SE');
 
